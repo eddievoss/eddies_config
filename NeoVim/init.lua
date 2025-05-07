42d8f7387f7535107a8901728bf41c8e0a8f084a
Eddie Voss <94473483+eddievoss@users.noreply.github.com>
2025-05-06 21:55:31 -0600

    Update init.lua

--- NeoVim/init.lua
+++ NeoVim/init.lua
@@ -1,1 +1,26 @@
+-- Use system clipboard for all yanks and pastes
+vim.opt.clipboard = "unnamedplus"
 
+-- Force y, d, p, and similar commands to use the system clipboard
+local map  = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+map("n", "y",  '"+y',  opts)
+map("n", "yy", '"+yy', opts)
+map("n", "p",  '"+p',  opts)
+map("n", "P",  '"+P',  opts)
+map("n", "d",  '"+d',  opts)
+map("n", "dd", '"+dd', opts)
+
+map("v", "y", '"+y', opts)
+map("v", "d", '"+d', opts)
+map("v", "p", '"+p', opts)
+
+-- Show the command as you type (e.g. `d$`, `y$`, …)
+vim.opt.showcmd = true
+
+-- Show the current mode (e.g. -- INSERT --)
+vim.opt.showmode = true
+
+-- Suppress unwanted messages
+vim.opt.shortmess:append("cI")
